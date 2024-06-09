22f74a2e522b2fbf5c7c623549e35b586f9fcbc0
Archimede Body <archimedebody@rocketmail.com>
2024-06-09 11:23:17 +0300

    chore: Update NestJS dependencies to version 10.0.1

--- dist/src/interceptors/http.interceptor.js
+++ dist/src/interceptors/http.interceptor.js
@@ -1,11 +1,17 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.HTTPInterceptor = void 0;
 const common_1 = require("@nestjs/common");
 const rxjs_1 = require("rxjs");
 const operators_1 = require("rxjs/operators");
 const log_1 = require("../utils/log");
-class HTTPInterceptor {
+let HTTPInterceptor = class HTTPInterceptor {
     intercept(context, next) {
         const startTime = Date.now();
         const http = context.switchToHttp();
@@ -50,6 +56,9 @@ class HTTPInterceptor {
             return `${statusCode}`;
         }
     }
-}
+};
+HTTPInterceptor = __decorate([
+    (0, common_1.Injectable)()
+], HTTPInterceptor);
 exports.HTTPInterceptor = HTTPInterceptor;
 //# sourceMappingURL=http.interceptor.js.map
